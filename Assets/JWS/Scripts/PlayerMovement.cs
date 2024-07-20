179e3952dd291ad6585a572cda1e07992196fecd
jws2170 <mpart2077@gmail.com>
2024-07-20 13:54:25 +0900

    [ADD] Asset

--- Assets/JWS/Scripts/PlayerMovement.cs
+++ Assets/JWS/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using Unity.IO.LowLevel.Unsafe;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,12 +11,13 @@ public class PlayerMovement : MonoBehaviour
     [Header("PlayerSetting")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpPower = 10f;
+    [SerializeField] private GameObject charactorHolder = null;
 
     private Rigidbody2D rb = null;
     private Animator animator = null;
     private float horizontal = default;
     private Vector2 dir = Vector2.zero;
-    private bool faceLeft = default;
+    private bool faceRight = default;
     public bool isGround = default;
     private bool isJump = default;
 
@@ -23,6 +25,11 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        faceRight = true;
     }
 
     private void Update()
@@ -42,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
         if (isGround && Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            StartCoroutine(JumpSqueeze(0.5f, 1.2f, 0.1f));
             isJump = true;
         }
         if (isJump && Input.GetKeyUp(KeyCode.Space))
@@ -49,6 +57,11 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
             isJump = false;
         }
+        if(rb.velocity.y < 0f && !isGround)
+        {
+            rb.gravityScale = 2f;
+        }
+
     }
 
     private void PlayerMove()
@@ -63,18 +76,39 @@ public class PlayerMovement : MonoBehaviour
 
     private void PlayerFlip()
     {
-        if ((horizontal > 0 && !faceLeft) || (horizontal < 0 && faceLeft))
+        if ((horizontal > 0 && !faceRight) || (horizontal < 0 && faceRight))
         {
-            faceLeft = !faceLeft;
-            transform.rotation = Quaternion.Euler(0, faceLeft ? 180 : 0, 0);
+            faceRight = !faceRight;
+            transform.rotation = Quaternion.Euler(0, faceRight ? 0 : 180, 0);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private IEnumerator JumpSqueeze(float xSquezze, float ySquezze, float seconds)
+    {
+        Vector3 originalSize = Vector3.one;
+        Vector3 newSize = new Vector3(xSquezze, ySquezze, originalSize.z);
+        float t = 0;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            charactorHolder.transform.localScale = Vector3.Lerp(originalSize, newSize, t);
+            yield return null;
+        }
+        t = 0;
+        while((t <= 1.0))
+        {
+            t += Time.deltaTime / seconds;
+            charactorHolder.transform.localScale = Vector3.Lerp(newSize, originalSize, t);
+            yield return null;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGround = true;
+            rb.gravityScale = 1f;
         }
     }
 
