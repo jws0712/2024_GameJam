eeaf6a887b88e62fc021b1c6b6f66413b7be1155
jws2170 <mpart2077@gmail.com>
2024-07-20 11:02:27 +0900

    [ADD] File

---
+++ Assets/JWS/Scripts/PlayerMovement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("PlayerSetting")]
+    [SerializeField] private float moveSpeed = default;
+    [SerializeField] private float jummpPower = default;
+}
